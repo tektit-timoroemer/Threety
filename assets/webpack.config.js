d45911938afcbf07fae8e8d402c82e67031d330c
Wilfried Römer <wilfried.roemer@web.de>
2021-01-14 11:40:20 +0100

    fixed webpack error reading hash for app.scss

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -49,6 +49,23 @@ module.exports = (env, options) => {
       new MiniCssExtractPlugin({ filename: '../css/app.css' }),
       new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
     ]
-    .concat(devMode ? [new HardSourceWebpackPlugin()] : [])
+    // modified according to
+    // https://github.com/mzgoddard/hard-source-webpack-plugin/issues/416
+    // because of error message
+    // "Could not freeze ./css/app.scss: Cannot read property 'hash' of undefined"
+    // WR 2021-01-14
+    .concat(devMode ? [
+      new HardSourceWebpackPlugin(),
+      new HardSourceWebpackPlugin.ExcludeModulePlugin([
+        {
+          // HardSource works with mini-css-extract-plugin but due to how
+          // mini-css emits assets, assets are not emitted on repeated builds with
+          // mini-css and hard-source together. Ignoring the mini-css loader
+          // modules, but not the other css loader modules, excludes the modules
+          // that mini-css needs rebuilt to output assets every time.
+          test: /mini-css-extract-plugin[\\/]dist[\\/]loader/,
+        },
+      ]),
+      ] : [])
   }
 };
